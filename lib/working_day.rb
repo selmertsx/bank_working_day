3b5b18b425830018fd21b77a33344fde4b9c4fb4
Morioka Shuhei <selmertsx@gmail.com>
2016-03-05 10:18:36 +0900

    add test end_of_month_witout_holiday

--- lib/working_day.rb
+++ lib/working_day.rb
@@ -1,6 +1,9 @@
-require "working_day/version"
+require 'working_day/version'
 require 'active_support'
 require 'active_support/core_ext'
+require 'pry'
 
 module WorkingDay
+  def self.end_of_month_without_holiday(year: , month: )
+  end
 end
--- spec/working_day_spec.rb
+++ spec/working_day_spec.rb
@@ -5,7 +5,13 @@ describe WorkingDay do
     expect(WorkingDay::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context '.end_of_month_without_holiday' do
+    subject { WorkingDay.end_of_month_without_holiday(year: year, month: month) }
+    let(:year) { 2016 }
+    let(:month) { 3 }
+
+    it 'when year and month was given' do
+      is_expected.to eq (31)
+    end
   end
 end
