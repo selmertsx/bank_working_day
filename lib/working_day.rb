38957eb8f66bc0ded2a1deb04d142b447d740de3
Morioka Shuhei <selmertsx@gmail.com>
2016-03-05 09:54:48 +0900

    initial commit

---
+++ lib/working_day.rb
@@ -0,0 +1,6 @@
+require "working_day/version"
+require 'active_support'
+require 'active_support/core_ext'
+
+module WorkingDay
+end
---
+++ spec/working_day_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe WorkingDay do
+  it 'has a version number' do
+    expect(WorkingDay::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
