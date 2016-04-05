61ee74dff9e00d348569d5552d88ae37b0b1d7d1
Morioka Shuhei <selmertsx@gmail.com>
2016-04-05 13:58:02 +0900

    add deduction_date methods
    bump version 0.1.4

--- lib/bank_working_day.rb
+++ lib/bank_working_day.rb
@@ -29,6 +29,17 @@ module BankWorkingDay
     date
   end
 
+  def self.deduction_date(year: , month: , day: 27)
+    raise InvalidArgumentError if year.to_i.zero? || month.to_i.zero?
+
+    date = Date.new(year.to_i, month.to_i, day.to_i)
+    while holiday?(date)
+      date += 1
+    end
+
+    date
+  end
+
   def self.holiday?(date)
     holidays.holiday?(date)
   end
--- lib/bank_working_day/version.rb
+++ lib/bank_working_day/version.rb
@@ -1,3 +1,3 @@
 module BankWorkingDay
-  VERSION = "0.1.2"
+  VERSION = "0.1.4"
 end
