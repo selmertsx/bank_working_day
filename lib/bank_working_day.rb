2a5ba6e8faaf6f061cf509b9be3342df94a4cb72
sosuke-takahashi <s-takahashi@tes.co.jp>
2019-02-04 18:10:22 +0900

    Change module methods to instance methods

--- lib/bank_working_day.rb
+++ lib/bank_working_day.rb
@@ -6,49 +6,51 @@ require 'bank_working_day/holidays'
 module BankWorkingDay
   class InvalidArgumentError < StandardError; end
 
-  def self.end_of_month_without_holiday(date)
-    date.end_of_month.day.downto(1) do |last_day|
-      last_day = Date.new(date.year, date.month, last_day)
-      return last_day unless holidays.holiday?(last_day)
-    end
-  end
+  class BankWorkingDay
+    attr_accessor :holidays
 
-  def self.working_day_before(date: , offset: )
-    offset.times do |i|
-      date -= 1
-      date -= 1 while holiday?(date)
+    def initialize(holidays_yml_path='../../../holidays.yml')
+      @holidays ||= Holidays.new(holidays_yml_path)
     end
 
-    date
-  end
-
-  def self.working_day_after(date: , offset: )
-    offset.times do |i|
-      date += 1
-      date += 1 while holiday?(date)
+    def end_of_month_without_holiday(date)
+      date.end_of_month.day.downto(1) do |last_day|
+        last_day = Date.new(date.year, date.month, last_day)
+        return last_day unless holiday?(last_day)
+      end
     end
 
-    date
-  end
+    def working_day_before(date: , offset: )
+      offset.times do |i|
+        date -= 1
+        date -= 1 while holiday?(date)
+      end
 
-  def self.deduction_date(year: , month: , day: 27)
-    raise InvalidArgumentError if year.to_i.zero? || month.to_i.zero?
-
-    date = Date.new(year.to_i, month.to_i, day.to_i)
-    while holiday?(date)
-      date += 1
+      date
     end
 
-    date
-  end
+    def working_day_after(date: , offset: )
+      offset.times do |i|
+        date += 1
+        date += 1 while holiday?(date)
+      end
 
-  def self.holiday?(date)
-    holidays.holiday?(date)
-  end
+      date
+    end
 
-  private
+    def deduction_date(year: , month: , day: 27)
+      raise InvalidArgumentError if year.to_i.zero? || month.to_i.zero?
 
-  def self.holidays
-    @holidays ||= Holidays.new
+      date = Date.new(year.to_i, month.to_i, day.to_i)
+      while holiday?(date)
+        date += 1
+      end
+
+      date
+    end
+
+    def holiday?(date)
+      holidays.holiday?(date)
+    end
   end
 end
--- lib/bank_working_day/holidays.rb
+++ lib/bank_working_day/holidays.rb
@@ -14,8 +14,8 @@ module BankWorkingDay
 
     HOLIDAY_WDAYS = [0, 6].freeze
 
-    def initialize
-      @holidays = set_holiday(holidays_path)
+    def initialize(holidays_yml_path)
+      @holidays = set_holiday(holidays_yml_path)
       @bank_holidays = set_holiday('../../../bank_holidays.yml')
     end
 
@@ -24,12 +24,6 @@ module BankWorkingDay
     end
 
     private
-
-    # Rails側に祝日のYAMLファイルがあれば優先する
-    def holidays_path
-      path = Rails.root.to_s + '/config/holidays.yml'
-      File.exist?(path) ? path : '../../../holidays.yml'
-    end
 
     def set_holiday(path)
       yaml = YAML.load_file(File.expand_path(path, __FILE__))
